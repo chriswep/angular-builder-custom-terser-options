200eb12c07c82e5d6213ea163487a6692778e0b6
Chris <chriswep@mcloud.email>
2019-07-11 17:50:47 +0200

    added possibility to set regEx terser options

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,9 +1,13 @@
 import { BrowserBuilder } from '@angular-devkit/build-angular'
-import TerserPlugin = require('terser-webpack-plugin')
 
 export default class CustomizeTerserBrowserBuilder extends BrowserBuilder {
   public buildWebpackConfig(root: any, projectRoot: any, host: any, options: any) {
     const terserOptionsCustom = options.terserOptions
+    if (typeof terserOptionsCustom.keep_fnames === 'string')
+      terserOptionsCustom.keep_fnames = new RegExp(terserOptionsCustom.keep_fnames)
+    if (typeof terserOptionsCustom.keep_classnames === 'string')
+      terserOptionsCustom.keep_classnames = new RegExp(terserOptionsCustom.keep_classnames)
+
     const webpackConfig = super.buildWebpackConfig(root, projectRoot, host, options)
     if (
       terserOptionsCustom &&
@@ -12,7 +16,7 @@ export default class CustomizeTerserBrowserBuilder extends BrowserBuilder {
       Array.isArray(webpackConfig.optimization.minimizer)
     ) {
       const terserPlugin = (webpackConfig.optimization.minimizer as any[]).find(
-        (minimizer) => minimizer instanceof TerserPlugin,
+        (minimizer) => minimizer.options && minimizer.terserOptions,
       )
       if (terserPlugin) {
         const terserOptionsOriginal = terserPlugin.options.terserOptions
